be76f46004afb45eb7beb8775de5ecb0fe6fa849
DuckDivers <howard@duckdiverllc.com>
2019-09-29 09:08:09 -0400

    Initialize Plugin

---
+++ dd-cf7-constant-contact-v3.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://www.howardehrenberg.com
+ * @since             1.0.0
+ * @package           dd_cf7_constant_contact_v3
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Contact Form 7 to Constant Contact V3
+ * Plugin URI:        https://www.duckdiverllc.com
+ * Description:       Connect Contact form 7 and Constant Contact where it appends existing users - allows for multiple list subscriptions, and conditional subscribe checkbox.
+ * Version:           1.0.0
+ * Author:            Howard Ehrenberg
+ * Author URI:        https://www.howardehrenberg.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       dd-cf7-plugin
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'DD_CF7_CONSTANT_CONTACT_V3_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-dd-cf7-constant-contact-v3-activator.php
+ */
+function activate_dd_cf7_constant_contact_v3() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-dd-cf7-constant-contact-v3-activator.php';
+	dd_cf7_constant_contact_v3_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-dd-cf7-constant-contact-v3-deactivator.php
+ */
+function deactivate_dd_cf7_constant_contact_v3() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-dd-cf7-constant-contact-v3-deactivator.php';
+	dd_cf7_constant_contact_v3_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_dd_cf7_constant_contact_v3' );
+register_deactivation_hook( __FILE__, 'deactivate_dd_cf7_constant_contact_v3' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-dd-cf7-constant-contact-v3.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_dd_cf7_constant_contact_v3() {
+
+	$plugin = new dd_cf7_constant_contact_v3();
+	$plugin->run();
+
+}
+run_dd_cf7_constant_contact_v3();
---
+++ public/class-dd-cf7-constant-contact-v3-public.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://www.howardehrenberg.com
+ * @since      1.0.0
+ *
+ * @package    dd_cf7_constant_contact_v3
+ * @subpackage dd_cf7_constant_contact_v3/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    dd_cf7_constant_contact_v3
+ * @subpackage dd_cf7_constant_contact_v3/public
+ * @author     Howard Ehrenberg <howard@howardehrenberg.com>
+ */
+class dd_cf7_constant_contact_v3_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in dd_cf7_constant_contact_v3_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The dd_cf7_constant_contact_v3_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dd-cf7-constant-contact-v3-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in dd_cf7_constant_contact_v3_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The dd_cf7_constant_contact_v3_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dd-cf7-constant-contact-v3-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
